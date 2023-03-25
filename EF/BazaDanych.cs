e4839071a49a7394eb003ec71a43c3d23e1a71d7
FilipSzerszen <filipsz60@wp.pl>
2023-03-25 20:43:55 +0100

    Dodaj pliki projektów.

---
+++ EF/BazaDanych.cs
@@ -0,0 +1,52 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EF
+{
+    public class BazaDanychOsóbDBContext : DbContext
+    {
+        public DbSet<Osoba> Osoby { get; set; }
+        public DbSet<Adres> Adres { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=osoby.db");
+        }
+    }
+    public class BazaDanychOsób : IDisposable
+    {
+        private BazaDanychOsóbDBContext dbc = new BazaDanychOsóbDBContext();
+        public BazaDanychOsób()
+        {
+            dbc.Database.EnsureCreated();
+        }
+        public void Dispose()
+        {
+            dbc.Dispose();
+        }
+#if DEBUG
+        public Osoba[] Osoby { get => dbc.Osoby.ToArray(); }
+        public Adres[] Adresy { get => dbc.Adres.ToArray(); }
+#endif
+        public Osoba PobierzOsobę(int idOsoby)
+        {
+            return dbc.Osoby.FirstOrDefault(o => o.Id == idOsoby);
+        }
+        public Osoba this[int idOsoby]
+        {
+            get => PobierzOsobę(idOsoby);
+        }
+        public int[] IdentyfikatoryOsób
+        {
+            get
+            {
+                return dbc.Osoby.Select(o => o.Id).ToArray();
+            }
+        }
+        public int DodajOsobę(Osoba osoba)
+        {
+
+            return 2;
+        }
+    }
+}
---
+++ EF/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using EF;
+
+
+namespace EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adres adres1 = new Adres()
+            {
+                Id = 1,
+                Miasto = "Wrocław",
+                Ulica = "Borowska",
+                NumerDomu = 38,
+                NumerMieszkania = 10
+            };
+            Adres adres2 = new Adres()
+            {
+                Id = 2,
+                Miasto = "Wrocław",
+                Ulica = "Borowska",
+                NumerDomu = 38,
+                NumerMieszkania = 10
+            };
+            Console.WriteLine(adres1.Equals(adres2));
+        }
+
+    }
+}

ada9dec9ebd89cce48a42115bc097b7ac24504a7
FilipSzerszen <filipsz60@wp.pl>
2023-03-27 13:49:28 +0200

    Dodano opcję dodawania osób, modyfikacji osób i ich usuwania

--- EF/BazaDanych.cs
+++ EF/BazaDanych.cs
@@ -1,13 +1,11 @@
-﻿
-
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace EF
 {
     public class BazaDanychOsóbDBContext : DbContext
     {
         public DbSet<Osoba> Osoby { get; set; }
-        public DbSet<Adres> Adres { get; set; }
+        public DbSet<Adres> Adresy { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=osoby.db");
@@ -26,15 +24,16 @@ namespace EF
         }
 #if DEBUG
         public Osoba[] Osoby { get => dbc.Osoby.ToArray(); }
-        public Adres[] Adresy { get => dbc.Adres.ToArray(); }
+        public Adres[] Adresy { get => dbc.Adresy.ToArray(); }
 #endif
-        public Osoba PobierzOsobę(int idOsoby)
+        public Osoba? PobierzOsobę(int idOsoby)
         {
             return dbc.Osoby.FirstOrDefault(o => o.Id == idOsoby);
         }
-        public Osoba this[int idOsoby]
+        public Osoba? this[int idOsoby]
         {
             get => PobierzOsobę(idOsoby);
+            set => ZmieńDaneOsoby(idOsoby, value);
         }
         public int[] IdentyfikatoryOsób
         {
@@ -45,8 +44,59 @@ namespace EF
         }
         public int DodajOsobę(Osoba osoba)
         {
+            if (osoba == null) throw new ArgumentNullException("Podano pustą referencję jako argument");
 
-            return 2;
+            if (osoba.Adres == null) throw new ArgumentNullException("Osoba nie ma adresu, nie jest zameldowana");
+
+            if (dbc.Osoby.ToArray().Any(o => o.Equals(osoba))) return osoba.Id;
+            else  if (dbc.Osoby.Any(o => o.Id == osoba.Id)) osoba.Id = dbc.Osoby.Max(o => o.Id) + 1; 
+
+            Adres adres = dbc.Adresy.ToArray().FirstOrDefault(a=>a.Equals(osoba.Adres));
+            if (adres != null) osoba.Adres = adres;
+
+            dbc.Osoby.Add(osoba);
+            dbc.SaveChanges();
+            return osoba.Id;
+        }
+        private int[] PobierzIdentyfikatoryUżywanychAdresów()
+        {
+            return dbc.Osoby.Select(o => o.Adres.Id).Distinct().ToArray();
+        }
+        private Adres[] PobierzNieużywaneAdresy()
+        {
+            int[] używaneIdentyfikatoryAdresów = PobierzIdentyfikatoryUżywanychAdresów();
+            List<Adres> nieużywaneAdresy = new List<Adres>();
+            foreach(Adres adres in dbc.Adresy)
+            {
+                if (!używaneIdentyfikatoryAdresów.Contains(adres.Id)) nieużywaneAdresy.Add(adres);
+            }
+            return nieużywaneAdresy.ToArray();
+        }
+        private void usuńNieużywaneAdresy()
+        {
+            dbc.Adresy.RemoveRange(PobierzNieużywaneAdresy());
+            dbc.SaveChanges();
+        }
+        public void UsuńOsobę(int idOsoby)
+        {
+            Osoba osoba = PobierzOsobę(idOsoby);
+            if (osoba != null)
+            {
+                dbc.Osoby.Remove(osoba);
+                dbc.SaveChanges();
+                usuńNieużywaneAdresy();
+            }
+        }
+        public void ZmieńDaneOsoby(int idOsoby,Osoba noweDane)
+        {
+            Osoba osoba = PobierzOsobę(idOsoby);
+            if (osoba == null) throw new Exception("Nie ma osoby o podanym identyfikatorze");
+            osoba.Imię = noweDane.Imię;
+            osoba.Nazwisko = noweDane.Nazwisko;
+            osoba.NumerTelefonu = noweDane.NumerTelefonu;
+            osoba.Adres = noweDane.Adres;
+
+            dbc.SaveChanges();
         }
     }
 }
--- EF/Program.cs
+++ EF/Program.cs
@@ -8,6 +8,45 @@ namespace EF
     {
         static void Main(string[] args)
         {
+            string dbName = "osoby.db";
+            if (File.Exists(dbName)) File.Delete(dbName);
+
+            BazaDanychOsób db = new BazaDanychOsób();
+
+            dodajPrzykładoweOsoby(db);
+            podglądBazyDanych(db);
+
+            pokażIdentyfikatoryOsób(db);
+            pokażOsoby(db);
+
+
+
+            db[1] = new Osoba() { Imię = "Jan", Nazwisko = "Nowak", Adres = new Adres { Miasto = "Toruń", Ulica = "Mostowa", NumerDomu = 3, NumerMieszkania = 5 } };
+            podglądBazyDanych(db);
+
+            int idUsuwanejOsoby = 3;
+            Console.WriteLine($"Usuwam osobę numer {idUsuwanejOsoby}...");
+            db.UsuńOsobę(idUsuwanejOsoby);
+            podglądBazyDanych(db);
+        }
+        static void podglądBazyDanych(BazaDanychOsób db)
+        {
+            try
+            {
+                Console.WriteLine("Podgląd danych: ");
+                Console.WriteLine("Osoby: ");
+                foreach (Osoba osoba in db.Osoby) Console.WriteLine(osoba);
+                Console.WriteLine("Adresy: ");
+                foreach (Adres adres in db.Adresy) Console.WriteLine(adres);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Błąd podglądu bazy danych: " + ex.ToString());
+            }
+
+        }
+        static void dodajPrzykładoweOsoby(BazaDanychOsób db)
+        {
             Adres adres1 = new Adres()
             {
                 Id = 1,
@@ -20,12 +59,47 @@ namespace EF
             {
                 Id = 2,
                 Miasto = "Wrocław",
-                Ulica = "Borowska",
-                NumerDomu = 38,
-                NumerMieszkania = 10
+                Ulica = "Bardzka",
+                NumerDomu = 39,
+                NumerMieszkania = 11
             };
-            Console.WriteLine(adres1.Equals(adres2));
+            Osoba osoba1 = new Osoba()
+            {
+                Imię = "Marian",
+                Nazwisko = "Paździoch",
+                NumerTelefonu = 123456,
+                Adres = adres1
+            };
+            Osoba osoba2 = new Osoba()
+            {
+                Imię = "Ferdynand",
+                Nazwisko = "Kiepski",
+                NumerTelefonu = 987654,
+                Adres = adres2
+            };
+            Osoba osoba3 = new Osoba()
+            {
+                Imię = "Halina",
+                Nazwisko = "Kiepska",
+                NumerTelefonu = 3456789,
+                Adres = adres2
+            };
+            db.DodajOsobę(osoba1);
+            db.DodajOsobę(osoba2);
+            db.DodajOsobę(osoba3);
         }
-
+        static void pokażIdentyfikatoryOsób(BazaDanychOsób db)
+        {
+            string s = "Identyfikatory osób: ";
+            foreach (int idOsoby in db.IdentyfikatoryOsób) s += idOsoby.ToString() + "; ";
+            Console.WriteLine(s.TrimEnd(' ', ';'));
+        }
+        static void pokażOsoby(BazaDanychOsób db)
+        {
+            foreach (int idOsoby in db.IdentyfikatoryOsób)
+            {
+                Console.WriteLine(db[idOsoby].ToString());
+            }
+        }
     }
 }
